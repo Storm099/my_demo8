13a7de28b47f3b908ae33ded998e53082c135340
Storm099 <Yorqinbekabrayev1999@gmail.com>
2021-09-24 23:19:03 +0300

    Learn drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,44 +8,121 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void _openDialog() {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Log Out"),
-            content: Text("Are sure to exit"),
-            actions: [
-              FlatButton(
-                color: Colors.pink,
-                child: Text("cancel"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              FlatButton(
-                color: Colors.cyanAccent,
-                child: Text("confirm"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          );
-        });
-  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Storm"),
+        centerTitle: true,
+      ),
       body: Center(
-        child: FlatButton(
-          child: Text("open dialog"),
-          color: Colors.cyanAccent,
-          onPressed: () {
-            _openDialog();
-          },
-        ),
+        child: Text("Hi Flutter from Storm"),
+
+      ),
+      drawer: Drawer(
+       child: Column(
+         crossAxisAlignment: CrossAxisAlignment.stretch,
+         children: [
+           Expanded(
+             flex: 2,
+             child: Container(
+               color: Colors.blue,
+               padding: EdgeInsets.all(20),
+               child: Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+
+                   Container(
+                     height: 70,
+                     width: 70,
+                     decoration: BoxDecoration(
+                       shape: BoxShape.circle,
+                       image: DecorationImage(
+                         fit: BoxFit.cover,
+                         image: AssetImage("assets/images/mypic2.jpg")
+                       )
+                     ),
+                   ),
+                   Text("Yorqin Abrayev",style: TextStyle(color: Colors.white,fontSize: 18),),
+                   SizedBox(height: 5,),
+                   Text("Yorqinbekabrayev1999@gmail.com",style: TextStyle(color: Colors.white),)
+
+                 ],
+               ),
+
+             ),
+           ),
+
+           Expanded(
+             flex: 4,
+             child: Container(
+               color: Colors.white,
+               child: Column(
+                 children: [
+                   Container(
+                     height: 60,
+                     color: Colors.white,
+                     child: Row(
+                       children: [
+                         Padding(
+                           padding: const EdgeInsets.all(8.0),
+                           child: Icon(
+                             (Icons.home),
+                           ),
+                         ),
+                         SizedBox(width: 10,),
+                         Text("Home")
+
+                       ],
+                     ),
+                   ),
+                   Container(
+                     height: 60,
+                     color: Colors.white,
+                     child: Row(
+                       children: [
+                         Padding(
+                           padding: const EdgeInsets.all(8.0),
+                           child: Icon(
+                             (Icons.account_circle),
+                           ),
+                         ),
+                         SizedBox(width: 10,),
+                         Text("Profile")
+
+                       ],
+                     ),
+                   ),
+                   Container(
+                     height: 60,
+                     color: Colors.white,
+                     child: Row(
+                       children: [
+                         Padding(
+                           padding: const EdgeInsets.all(8.0),
+                           child: Icon(
+                             (Icons.people),
+                           ),
+                         ),
+                         SizedBox(width: 10,),
+                         Text("About us")
+
+                       ],
+                     ),
+                   )
+
+
+                 ],
+               ),
+
+             ),
+           )
+
+         ],
+       ),
       ),
     );
   }
